c1ea8867b3b4f705714e62928e3052c9342f4cb0
Meet Rojivadiya <meetbr26@gmail.com>
2022-02-22 20:00:51 +0530

    Added _document.js file

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import {Html,Head,Main,NextScript} from 'next/document'
+
+export default function Document(){
+    return (
+        <Html>
+            <Head>
+                <title>The Mealeria - Next App</title>
+            </Head>
+            <body>
+                <Main>
+                </Main>
+                <NextScript></NextScript>
+            </body>
+        </Html>
+    )
+}
